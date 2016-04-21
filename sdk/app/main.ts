05883159d8f5bf164f83392647034bcc21fd9636
sophialazarova <sophia.lazarova@yahoo.com>
2016-04-21 16:04:05 +0300

    position example sidedrawer

--- sdk/app/main.ts
+++ sdk/app/main.ts
@@ -1,7 +1,15 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
+<<<<<<< Updated upstream
 import {AppComponent} from "./listview/header-footer/listview-header-footer.component";
 import listViewModule = require('nativescript-telerik-ui-pro/listview');
 import {LISTVIEW_PROVIDERS} from 'nativescript-telerik-ui-pro/listview/angular/listview-directives'
+=======
+import {AppComponent} from "./sidedrawer/getting-started/getting-started.component";
+//import listViewModule = require('nativescript-telerik-ui-pro/listview');
+import {SIDEDRAWER_PROVIDERS} from "nativescript-telerik-ui-pro/sidedrawer/angular/side-drawer-directives"
+import application = require("nativescript-angular/application");
+application.cssFile = "./app.css";
+>>>>>>> Stashed changes
 
-nativeScriptBootstrap(AppComponent, LISTVIEW_PROVIDERS);
+nativeScriptBootstrap(AppComponent);
