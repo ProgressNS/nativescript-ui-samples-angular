e00de5fd2b907f7e7b6112e8824e4e34ae21ca7e
Deyan Ginev <deyan.ginev@telerik.com>
2016-04-21 11:55:59 +0300

    Examples added

---
+++ sdk/app/listview/header-footer/listview-header-footer.component.ts
@@ -0,0 +1,41 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: "my-app",
+    template: `
+<GridLayout rows="auto, *">
+    <RadListView row="1" [items]="myItems">
+        <template listItemTemplate #item="item">
+            <StackLayout><Label [text]='item.name'></Label></StackLayout>
+        </template>
+        <StackLayout *listViewHeader backgroundColor="red">
+            <Label text="This is the list header"></Label>
+        </StackLayout>
+        <StackLayout *listViewFooter backgroundColor="blue">
+            <Label text="This is the list footer"></Label>
+        </StackLayout>
+    </RadListView>
+</GridLayout>
+`
+})
+export class AppComponent {
+    private _items;
+
+    constructor() {
+        this._items = new Array();
+
+        for (var i = 0; i < 100; i++) {
+            this._items.push({
+                name: "Item" + i
+            });
+        }
+    }
+
+    get myItems() {
+        return this._items;
+    }
+
+    set myItems(value) {
+        this._items = value;
+    }
+}
--- sdk/app/main.ts
+++ sdk/app/main.ts
@@ -1,6 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
-import {AppComponent} from "./listview/item-reorder/listview-item-reorder.component";
+import {AppComponent} from "./listview/header-footer/listview-header-footer.component";
 import listViewModule = require('nativescript-telerik-ui-pro/listview');
 import {LISTVIEW_PROVIDERS} from 'nativescript-telerik-ui-pro/listview/angular/listview-directives'
 
