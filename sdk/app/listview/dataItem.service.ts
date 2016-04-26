97bb0abb4bce99690659f5b5357960a04b15bd6a
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-04-26 13:35:47 +0300

    Updated swipe to execute example. Updated package.json with missing dependencies.

--- sdk/app/listview/dataItem.service.ts
+++ sdk/app/listview/dataItem.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from 'angular2/core';
 import {DATAITEMS} from "./mock-dataItems";
 import {DataItem} from "./dataItem";
+import {Person} from "./Person";
 var namesAndEmails = require("../listview/NamesAndEmails.json")
+var posts = require("../listview/posts.json")
 
 
 @Injectable()
@@ -36,6 +38,15 @@ export class DataItemService {
         var result = new Array<DataItem>();
         for (var i = 0; i < namesAndEmails.names.length; i++) {
             result.push(new DataItem(i, namesAndEmails.names[i], namesAndEmails.emails[i]));
+        }
+        
+        return result;
+    }
+    
+    getPersonPosts():Person[] {
+        var result = new Array<Person>();
+        for (var i = 0; i < posts.names.length; i++) {
+            result.push(new Person(posts.names[i], posts.titles[i], posts.text[i]));
         }
         
         return result;
