4fd88e0951c372c96f16d3e81ef166135804eafb
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-04-26 18:23:32 +0300

    Added image resources. Updated listview-load-on-demand example. Added fresco plugin in dep fodler.

--- sdk/app/listview/load-on-demand/listview-load-on-demand.component.ts
+++ sdk/app/listview/load-on-demand/listview-load-on-demand.component.ts
@@ -1,40 +1,67 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
+import {ObservableArray} from "data/observable-array";
+import {DataItem} from "../dataItem";
+import {DataItemService} from "../dataItem.service";
+import listViewModule = require("nativescript-telerik-ui-pro/listview");
+import * as Application from "application";
+import *as Timer  from "timer";
+var posts = require("../../listview/posts.json")
 
 @Component({
     selector: "my-app",
-    template: `
-<GridLayout rows="auto, *">
-    <RadListView row="1" [items]="myItems" loadOnDemandMode="Auto" (loadMoreDataRequested)="onLoadOnDemandRequested()">
-        <template listItemTemplate #item="item">
-            <StackLayout height="80" backgroundColor="Green"><Label [text]='item.name'></Label></StackLayout>
-        </template>
-    </RadListView>
-</GridLayout>
-`
+    providers: [DataItemService],
+    templateUrl: "listview/load-on-demand/listview-load-on-demand.component.html",
+    styleUrls: ["listview/load-on-demand/listview-load-on-demand.component.css"]
 })
-export class AppComponent {
-    private _items;
+export class AppComponent implements OnInit {
+    private _dataItems: ObservableArray<DataItem>;
+    private _numberOfAddedItems;
 
-    constructor() {
-        this._items = new Array();
+    constructor(private _dataItemService: DataItemService) {
+    }
 
-        for (var i = 0; i < 20; i++) {
-            this._items.push({
-                name: "Item" + i
-            });
+    get dataItems(): ObservableArray<DataItem> {
+        return this._dataItems;
+    }
+
+    ngOnInit() {
+        //this._dataItems = new ObservableArray(this._dataItemService.getIdenticalDataItems(10));
+        this.initDataItems();
+        console.log("dateItems: " + this._dataItems.length);
+    }
+
+    public onLoadMoreItemsRequested(args: listViewModule.ListViewEventData) {
+        var that = new WeakRef(this);
+        Timer.setTimeout(function () {
+            var listView: listViewModule.RadListView = args.object;
+            var initialNumberOfItems = that.get()._numberOfAddedItems;
+            for (var i = that.get()._numberOfAddedItems; i < initialNumberOfItems + 2; i++) {
+                if (i > posts.names.length - 1) {
+                    listView.loadOnDemandMode = listViewModule.ListViewLoadOnDemandMode[listViewModule.ListViewLoadOnDemandMode.None];
+                    break;
+                }
+                var imageUri = Application.android ? posts.images[i].toLowerCase() : posts.images[i];
+                that.get()._dataItems.push(new DataItem(i ,posts.names[i], "This is item description", posts.titles[i], posts.text[i], "res://" + imageUri));
+                that.get()._numberOfAddedItems++;
+            }
+
+            listView.notifyLoadOnDemandFinished();
+        }, 1000);
+        args.returnValue = true;
+    }
+
+    private initDataItems() {
+        this._dataItems = new ObservableArray<DataItem>();
+        this._numberOfAddedItems = 0;
+        for (var i = 0; i < posts.names.length - 15; i++) {
+            this._numberOfAddedItems++;
+            if (Application.android) {
+                var item = new DataItem(i, posts.names[i], "This is item description", posts.titles[i], posts.text[i], "res://" + posts.images[i].toLowerCase());
+                this._dataItems.push(item);
+            }
+            else {
+                this._dataItems.push(new DataItem(i, posts.names[i], "This is item description", posts.titles[i], posts.text[i], "res://" + posts.images[i]));
+            }
         }
-    }
-
-    get myItems() {
-        return this._items;
-    }
-
-    set myItems(value) {
-        this._items = value;
-    }
-    
-    public onLoadOnDemandRequested(args){
-        console.log("Load on demand fired");
-        
     }
 }
