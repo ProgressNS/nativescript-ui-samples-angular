15941368aec3dae43ff7c63660a1255577883a33
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-05-04 15:18:36 +0300

    Removed comments Updated package.json files

--- sdkAngular/app/main.ts
+++ sdkAngular/app/main.ts
@@ -1,30 +1,16 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import elementRegistry = require('nativescript-angular/element-registry');
 import {FrescoDrawee} from "fresco/fresco";
-var application = require("application");
-
-// SideDrawer components and providers
-//import {AppComponent} from "./sidedrawer/getting-started/getting-started.component";
 import {SIDEDRAWER_PROVIDERS} from "nativescript-telerik-ui-pro/sidedrawer/angular/side-drawer-directives";
-
-// ListView components and providers
-//import {AppComponent} from "./listview/load-on-demand/listview-load-on-demand.component";
 import {LISTVIEW_PROVIDERS} from 'nativescript-telerik-ui-pro/listview/angular/listview-directives';
-
-//import applicationAngular = require("nativescript-angular/application");
-//applicationAngular.cssFile = "./app.css";
-
 import {MainComponent} from "./main/main.component";
+import * as applicationModule from "application";
 
-
-if (application.android) {
-    application.onLaunch = function (intent) {
-        com.facebook.drawee.backends.pipeline.Fresco.initialize(application.android.context);
+if (applicationModule.android) {
+    applicationModule.onLaunch = function (intent) {
+        com.facebook.drawee.backends.pipeline.Fresco.initialize(applicationModule.android.context);
     };
 }
 
 elementRegistry.registerElement("FrescoDrawee", () => FrescoDrawee);
-//nativeScriptBootstrap(AppComponent, LISTVIEW_PROVIDERS);
 nativeScriptBootstrap(MainComponent, [LISTVIEW_PROVIDERS, SIDEDRAWER_PROVIDERS]);
-//nativeScriptBootstrap(AppComponent, SIDEDRAWER_PROVIDERS);
