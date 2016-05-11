b50c8f67dfebbbc824efef653f32652d0fc500f4
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-05-11 10:19:11 +0300

    Updated 'sidedrawer getting started' component

--- sdkAngular/app/sidedrawer/getting-started/getting-started.component.ts
+++ sdkAngular/app/sidedrawer/getting-started/getting-started.component.ts
@@ -1,18 +1,21 @@
-import {Component, ElementRef, ViewChild, Inject} from "angular2/core";
-import {View} from "ui/core/view";
-import {RadSideDrawer} from "nativescript-telerik-ui-pro/sidedrawer";
-import {Page} from "ui/page";
-import {ActionItem} from "ui/action-bar";
+import { Component, ElementRef, ViewChild, Inject, OnInit, ChangeDetectorRef } from "angular2/core";
+import { View } from "ui/core/view";
+import { RadSideDrawer } from "nativescript-telerik-ui-pro/sidedrawer";
+import { Page } from "ui/page";
+import { ActionItem } from "ui/action-bar";
 import sideDrawerModule = require('nativescript-telerik-ui-pro/sidedrawer');
-import {RadSideDrawerComponent, SideDrawerType, MainTemplateDirective, DrawerTemplateDirective} from "nativescript-telerik-ui-pro/sidedrawer/angular/side-drawer-directives";
+import * as ObservableModule from "data/observable";
+import { RadSideDrawerComponent, SideDrawerType, MainTemplateDirective, DrawerTemplateDirective } from "nativescript-telerik-ui-pro/sidedrawer/angular/side-drawer-directives";
 
 @Component({
     selector: "sidedrawer-getting-started",
     directives: [RadSideDrawerComponent, MainTemplateDirective, DrawerTemplateDirective],
-    templateUrl: 'sidedrawer/getting-started/getting-started.component.html'
+    templateUrl: 'sidedrawer/getting-started/getting-started.component.html',
+    styleUrls: ['sidedrawer/getting-started/getting-started.component.css']
 })
-export class SidedrawerGettingStartedComponent {
-    constructor(@Inject(Page) private page: Page) {
+export class SidedrawerGettingStartedComponent extends ObservableModule.Observable implements OnInit {
+    constructor( @Inject(Page) private page: Page, private _changeDetectionRef: ChangeDetectorRef) {
+        super();
     }
 
     @ViewChild(RadSideDrawerComponent) public drawerComponent: RadSideDrawerComponent;
@@ -20,9 +23,15 @@ export class SidedrawerGettingStartedComponent {
 
     ngAfterViewInit() {
         this.drawer = this.drawerComponent.sideDrawer;
+        this._changeDetectionRef.detectChanges();
     }
-    
-    public openDrawer(){
+
+    ngOnInit() {
+        this.set("mainContentText", "SideDrawer for NativeScript can be easily setup in the XML definition of your page by defining main- and drawer-content. The component"
+            + " has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.");
+    }
+
+    public openDrawer() {
         this.drawer.showDrawer();
     }
 
