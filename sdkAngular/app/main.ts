4122be77ac69cc14ae4c26f18532c9b8ed31fbf0
Deyan Ginev <deyan.ginev@telerik.com>
2016-05-13 10:03:32 +0300

    Snippet for global directives

--- sdkAngular/app/main.ts
+++ sdkAngular/app/main.ts
@@ -1,9 +1,11 @@
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import elementRegistry = require('nativescript-angular/element-registry');
 import {FrescoDrawee} from "fresco/fresco";
+// >> using-global-directives
 import {SIDEDRAWER_PROVIDERS} from "nativescript-telerik-ui-pro/sidedrawer/angular/side-drawer-directives";
 import {LISTVIEW_PROVIDERS} from 'nativescript-telerik-ui-pro/listview/angular/listview-directives';
 import {MainComponent} from "./main/main.component";
+// >> (hide)
 import * as applicationModule from "application";
 
 if (applicationModule.android) {
@@ -11,6 +13,7 @@ if (applicationModule.android) {
         com.facebook.drawee.backends.pipeline.Fresco.initialize(applicationModule.android.context);
     };
 }
-
+// << (hide)
 elementRegistry.registerElement("FrescoDrawee", () => FrescoDrawee);
 nativeScriptBootstrap(MainComponent, [LISTVIEW_PROVIDERS, SIDEDRAWER_PROVIDERS]);
+// << using-global-directives
