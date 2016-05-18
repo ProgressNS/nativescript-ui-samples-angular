bc96bd44e97ab2300a32460591eb3e0a1d50942b
guliashki <adrian.guliashki@gmail.com>
2016-05-18 17:34:52 +0300

    Started working on programatic control

---
+++ sdkAngular/app/calendar/programatic-control/calendar-programatic-control.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "calendar-programatic-control",
+    templateUrl: "calendar-programatic-control.component.html"
+})
+export class RadCalendarProgramaticControlComponent {
+   
+}
