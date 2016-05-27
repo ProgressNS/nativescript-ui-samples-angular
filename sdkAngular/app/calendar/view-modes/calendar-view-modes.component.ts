f0147f53b68935f4a8fa5c50f09db33f05543016
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-05-27 18:08:28 +0300

    Added ActionBar to calendar-view-modes.component

--- sdkAngular/app/calendar/view-modes/calendar-view-modes.component.ts
+++ sdkAngular/app/calendar/view-modes/calendar-view-modes.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, Inject } from "@angular/core";
+import { Router } from "@angular/router-deprecated";
 import { Page } from "ui/page";
 import calendarModule = require("nativescript-telerik-ui-pro/calendar");
+import * as frameModule from "ui/frame";
 
 @Component({
     moduleId: module.id,
@@ -8,27 +10,45 @@ import calendarModule = require("nativescript-telerik-ui-pro/calendar");
     templateUrl: "calendar-view-modes.component.html"
 })
 export class CalendarViewModesComponent {
-    
-    constructor(@Inject(Page) private _page: Page) {
+
+    constructor( @Inject(Page) private _page: Page, private _router: Router) {
+        this._page.addEventListener("navigatingTo", this.onNavigationTo);
     }
-    
+
     onYearTap() {
-        var calendar:calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
+        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
         calendar.viewMode = calendarModule.CalendarViewMode.Year;
     }
-    
+
     onMonthNamesTap() {
-        var calendar:calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
+        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
         calendar.viewMode = calendarModule.CalendarViewMode.MonthNames;
     }
-    
-    onMonthTap() { 
-        var calendar:calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
+
+    onMonthTap() {
+        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
         calendar.viewMode = calendarModule.CalendarViewMode.Month;
     }
-    
+
     onWeekTap() {
-        var calendar:calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
+        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
         calendar.viewMode = calendarModule.CalendarViewMode.Week;
+    }
+
+    public onNavigationTo(args) {
+        console.log("onNavigationTo: Forward");
+        if (args.isBackNavigation) {
+            console.log("onNavigationTo: Backward");
+        }
+    }
+
+    public onOptionsTapped() {
+        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
+
+        this._router.navigate(["Options", { selectedIndex: 0, items: ["Week", "Month", "Month names", "Year"] }]);
+    }
+
+    public onNavigationButtonTap() {
+        frameModule.topmost().goBack();
     }
 }
