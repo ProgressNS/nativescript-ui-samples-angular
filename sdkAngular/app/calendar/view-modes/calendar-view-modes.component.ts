4599bdfd881f60e32904f2d9a9fa84f4d1c65051
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-05-30 15:23:45 +0300

    Updated "calendar-view-modes" component to use the "options.component" on ios

--- sdkAngular/app/calendar/view-modes/calendar-view-modes.component.ts
+++ sdkAngular/app/calendar/view-modes/calendar-view-modes.component.ts
@@ -1,8 +1,10 @@
 import { Component, ViewChild, Inject } from "@angular/core";
 import { Router } from "@angular/router-deprecated";
-import { Page } from "ui/page";
-import calendarModule = require("nativescript-telerik-ui-pro/calendar");
+import { Page, NavigatedData } from "ui/page";
+import { OptionsService } from "../../navigation/options/options.service";
+import { RadCalendar, CalendarViewMode } from "nativescript-telerik-ui-pro/calendar";
 import * as frameModule from "ui/frame";
+import * as applicationModule from "application";
 
 @Component({
     moduleId: module.id,
@@ -10,42 +12,78 @@ import * as frameModule from "ui/frame";
     templateUrl: "calendar-view-modes.component.html"
 })
 export class CalendarViewModesComponent {
+    private _optionsParamName: string;
+    private _selectedIndex: number;
+    private _calendar: RadCalendar;
 
-    constructor( @Inject(Page) private _page: Page, private _router: Router) {
-        this._page.addEventListener("navigatingTo", this.onNavigationTo);
-    }
-
-    onYearTap() {
-        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
-        calendar.viewMode = calendarModule.CalendarViewMode.Year;
-    }
-
-    onMonthNamesTap() {
-        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
-        calendar.viewMode = calendarModule.CalendarViewMode.MonthNames;
-    }
-
-    onMonthTap() {
-        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
-        calendar.viewMode = calendarModule.CalendarViewMode.Month;
+    constructor( @Inject(Page) private _page: Page, private _router: Router, private _optionsService: OptionsService) {
+        this._page.on("loaded", this.onLoaded, this);
+        
+        if (applicationModule.ios) {
+            this._page.on("navigatingTo", this.onNavigatingTo, this);
+            this._optionsParamName = "viewMode";
+            this._optionsService.paramName = this._optionsParamName;
+        }
     }
 
     onWeekTap() {
-        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
-        calendar.viewMode = calendarModule.CalendarViewMode.Week;
+        this._calendar.viewMode = CalendarViewMode.Week;
+        if (applicationModule.ios) {
+            this._selectedIndex = 0;
+        }
     }
 
-    public onNavigationTo(args) {
-        console.log("onNavigationTo: Forward");
+    onMonthTap() {
+        this._calendar.viewMode = CalendarViewMode.Month;
+        if (applicationModule.ios) {
+            this._selectedIndex = 1;
+        }
+    }
+
+
+    onMonthNamesTap() {
+        this._calendar.viewMode = CalendarViewMode.MonthNames;
+        if (applicationModule.ios) {
+            this._selectedIndex = 2;
+        }
+    }
+
+    onYearTap() {
+        this._calendar.viewMode = CalendarViewMode.Year;
+        if (applicationModule.ios) {
+            this._selectedIndex = 3;
+        }
+    }
+
+    public onLoaded() {
+        this._calendar = <RadCalendar>this._page.getViewById("calendar");
+    }
+
+    public onNavigatingTo(args) {
+        var data = args as NavigatedData;
         if (args.isBackNavigation) {
-            console.log("onNavigationTo: Backward");
+            if (this._optionsService.paramName == this._optionsParamName) {
+                switch (this._optionsService.paramValue) {
+                    case "Week":
+                        this._calendar.viewMode = CalendarViewMode.Week;
+                        break;
+                    case "Month":
+                        this._calendar.viewMode = CalendarViewMode.Month;
+                        break;
+                    case "Month names":
+                        this._calendar.viewMode = CalendarViewMode.MonthNames;
+                        break;
+                    case "Year":
+                        this._calendar.viewMode = CalendarViewMode.Year;
+                    default:
+                        break;
+                }
+            }
         }
     }
 
     public onOptionsTapped() {
-        var calendar: calendarModule.RadCalendar = <calendarModule.RadCalendar>this._page.getViewById("calendar");
-
-        this._router.navigate(["Options", { selectedIndex: 0, items: ["Week", "Month", "Month names", "Year"] }]);
+        this._router.navigate(["Options", { selectedIndex: this._selectedIndex, paramName: this._optionsParamName, items: ["Week", "Month", "Month names", "Year"] }]);
     }
 
     public onNavigationButtonTap() {
