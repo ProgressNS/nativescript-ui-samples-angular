354befb9e1cc4a2d5a9b7c856efcbaa46203b7ba
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-06-08 17:51:42 +0300

    Extended OptionsExampleBase with Observable

--- sdkAngular/app/options-example-base.ts
+++ sdkAngular/app/options-example-base.ts
@@ -1,10 +1,13 @@
 import * as frameModule from "ui/frame";
 import { Router } from "@angular/router-deprecated";
+import * as ObservableModule from "data/observable";
 
-export class OptionsExampleBase {
+export class OptionsExampleBase extends ObservableModule.Observable {
     protected navigationParameters;
     protected router: Router;
-    constructor() {}
+    constructor() {
+        super();
+    }
     
     public onOptionsTapped() {
         this.router.navigate(["Options", {selectedIndex: this.navigationParameters.selectedIndex, paramName: this.navigationParameters.paramName, items: this.navigationParameters.items }]);
