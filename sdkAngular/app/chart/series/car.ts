9c7077b921683cad2241946d99f84cd421175c1a
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-06-20 11:21:57 +0300

    Code snippets

--- sdkAngular/app/chart/series/car.ts
+++ sdkAngular/app/chart/series/car.ts
@@ -1,4 +1,6 @@
+// >> chart-angular-car
 export class Car {
     constructor(public Brand?: string, public Amount?: number) {
     }
 }
+// << chart-angular-car
--- sdkAngular/app/chart/series/currency.ts
+++ sdkAngular/app/chart/series/currency.ts
@@ -1,4 +1,6 @@
+// >> chart-angular-currency
 export class Currency {
     constructor(public Date?: string, public Open?: number, public Close?: number, public Low?: number, public High?: number) {
     }
 }
+// << chart-angular-currency
