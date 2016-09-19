fc9b3b5bc89f7cd3d32c7c58b3eb83043d646178
Vladimir Amiorkov <v.amiorkov@gmail.com>
2016-09-19 13:51:19 +0300

    Upgraded to latest nativescript-fresco plugin version

--- sdkAngular/app/app.module.ts
+++ sdkAngular/app/app.module.ts
@@ -28,7 +28,6 @@ if (applicationModule.android) {
         frescoModule.initialize();
     };
 }
-elementRegistryModule.registerElement("FrescoDrawee", () => frescoModule.FrescoDrawee);
 createRouteEntryArray(AppExampleComponents);
 // << (hide)
 
