ebd98c9f66a5e8c7e60cac621c26fbf6c4bfbce3
Vladimir Amiorkov <v.amiorkov@gmail.com>
2016-11-23 16:52:39 +0200

    Changed default Android Activity to support DataForm styles on lower API

--- sdkAngular/app/local.android.d.ts
+++ sdkAngular/app/local.android.d.ts
@@ -12,3 +12,30 @@ declare module java {
     }
 }
 
+declare module android {
+    module support {
+        module v7 {
+            module app {
+                class AppCompatActivity {
+                    protected onCreate(savedInstanceState: android.os.Bundle);
+                    protected onSaveInstanceState(outState: android.os.Bundle);
+                    protected onStart();
+                    protected onStop();
+                    protected onDestroy();
+                    protected onBackPressed();
+                    protected onActivityResult(requestCode: number, resultCode: number, data: android.content.Intent);
+                }
+            }
+        }
+    }
+
+    module os {
+        class Bundle {
+        }
+    }
+
+    module content {
+        class Intent {
+        }
+    }
+}
