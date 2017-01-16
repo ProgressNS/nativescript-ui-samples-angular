8f4b93ba24f705984c1e79dce1418b7c4e6917b1
Vladimir Amiorkov <v.amiorkov@gmail.com>
2017-01-16 11:20:25 +0200

    Upgraded to latest angualr versions

--- sdkAngular/app/navigation/app.component.ts
+++ sdkAngular/app/navigation/app.component.ts
@@ -18,17 +18,17 @@ export class AppComponent {
 export function createRouteEntryArray(components) {
   var service = new ExampleItemService;
   APP_ROUTES.push({ path: "", redirectTo: "examples-depth-1/root/root", pathMatch: 'full' });
-  APP_ROUTES.push({ path: "examples-depth-1/:parentTitle/:tappedTitle", component: ExamplesListDepth1Component, pathMatch: 'full' });
-  APP_ROUTES.push({ path: "examples-depth-2/:parentTitle/:tappedTitle", component: ExamplesListDepth2Component, pathMatch: 'full' });
-  APP_ROUTES.push({ path: "examples-depth-3/:parentTitle/:tappedTitle", component: ExamplesListDepth3Component, pathMatch: 'full' });
-  APP_ROUTES.push({ path: "options", component: OptionsComponent, pathMatch: 'full' });
+  APP_ROUTES.push({ path: "examples-depth-1/:parentTitle/:tappedTitle", component: ExamplesListDepth1Component });
+  APP_ROUTES.push({ path: "examples-depth-2/:parentTitle/:tappedTitle", component: ExamplesListDepth2Component });
+  APP_ROUTES.push({ path: "examples-depth-3/:parentTitle/:tappedTitle", component: ExamplesListDepth3Component });
+  APP_ROUTES.push({ path: "options", component: OptionsComponent });
 
   for (let component of components) {
     var exampleItem = service.getExampleItemByComponent(EXAMPLEITEMS, component.name);
     if (!exampleItem) {
       throw new Error("No ExampleItem named '" + component.name + "' found.");
     }
-    APP_ROUTES.push({ path: exampleItem.path, component: component, pathMatch: 'full', data: { title: exampleItem.title } });
+    APP_ROUTES.push({ path: exampleItem.path, component: component, data: { title: exampleItem.title } });
   }
 }
 
