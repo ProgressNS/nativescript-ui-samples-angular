6b591d877749f306fb37ed22b3db54fecb9de38c
Vladimir Amiorkov <v.amiorkov@gmail.com>
2017-04-25 13:24:44 +0300

    Updated code snippet

--- sdkAngular/app/app.module.ts
+++ sdkAngular/app/app.module.ts
@@ -1,14 +1,16 @@
+// >> using-global-directives
+// This import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import * as elementRegistryModule from 'nativescript-angular/element-registry';
-// >> using-global-directives
+
+// --- The built-in 'nativescript-telerik-ui-pro' modules
 import { SIDEDRAWER_DIRECTIVES } from "nativescript-telerik-ui-pro/sidedrawer/angular";
 import { LISTVIEW_DIRECTIVES } from 'nativescript-telerik-ui-pro/listview/angular';
 import { CALENDAR_DIRECTIVES } from 'nativescript-telerik-ui-pro/calendar/angular';
 import { CHART_DIRECTIVES } from 'nativescript-telerik-ui-pro/chart/angular';
 import { DATAFORM_DIRECTIVES } from 'nativescript-telerik-ui-pro/dataform/angular';
 
-// Not required imports, these are used by the nativescript-samples-angular SDK examples
+// Not required imports, these are used by the nativescript-samples-angular SDK examples - https://github.com/telerik/nativescript-ui-samples-angular
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NgModule } from "@angular/core";
@@ -19,11 +21,11 @@ import { ExampleItemService } from "./navigation/exampleItemService.service";
 import { ExamplesListDepth1Component, ExamplesListDepth2Component, ExamplesListDepth3Component } from "./navigation/examples-list/examples-list.component";
 import { OptionsComponent } from "./navigation/options/options.component";
 import { COMMON_DIRECTIVES } from './navigation/directives';
+import { TNSFrescoModule } from "nativescript-fresco/angular";
 
 // >> (hide)
 import * as applicationModule from "tns-core-modules/application";
 import * as frescoModule from "nativescript-fresco";
-import { TNSFrescoModule } from "nativescript-fresco/angular";
 
 if (applicationModule.android) {
     applicationModule.on("launch", () => {
@@ -67,9 +69,7 @@ createRouteEntryArray(AppExampleComponents);
         ExampleItemService
     ]
 })
-class AppModule {
-
-}
+class AppModule { }
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
 // << using-global-directives
