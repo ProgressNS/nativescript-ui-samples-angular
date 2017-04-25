1d2ec42af6b2c94010b8572ec260c4e45be3ca9f
Vladimir Amiorkov <v.amiorkov@gmail.com>
2017-04-25 14:56:37 +0300

    Updated snippet

--- sdkAngular/app/app.module.ts
+++ sdkAngular/app/app.module.ts
@@ -1,5 +1,4 @@
 // >> using-global-directives
-// This import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
