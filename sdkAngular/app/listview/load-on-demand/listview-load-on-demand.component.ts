67c917a625c120eb55cd45f4b21bee212bb8612c
Deyan Ginev <deyan.ginev@telerik.com>
2017-05-02 14:09:51 +0300

    fixed styling issue

--- sdkAngular/app/listview/load-on-demand/listview-load-on-demand.component.ts
+++ sdkAngular/app/listview/load-on-demand/listview-load-on-demand.component.ts
@@ -24,7 +24,6 @@ export class ListViewLoadOnDemandComponent implements OnInit {
     ngOnInit() {
         this.layout = new ListViewLinearLayout();
         this.layout.scrollDirection = "Vertical";
-        this.layout.itemHeight = 220;
         this.initDataItems();
         this._changeDetectionRef.detectChanges();
     }
