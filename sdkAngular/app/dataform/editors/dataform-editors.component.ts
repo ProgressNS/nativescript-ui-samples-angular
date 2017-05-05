5acf009fdd148d9e29ffcc9c139b5290f5e11c62
tgpetrov <Todor.Petrov@telerik.com>
2017-05-05 17:38:24 +0300

    added usage of converters in DF editors example

--- sdkAngular/app/dataform/editors/dataform-editors.component.ts
+++ sdkAngular/app/dataform/editors/dataform-editors.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { TicketOrder } from "../data-services/ticket-order";
+import { Movie } from "../data-services/movie";
+import { MovieConverter } from "../data-services/movie-converter";
 
 @Component({
     moduleId: module.id,
@@ -8,20 +10,36 @@ import { TicketOrder } from "../data-services/ticket-order";
 })
 export class DataFormEditorsComponent implements OnInit {
     private _ticketOrder: TicketOrder;
-    private _movies: Array<String>;
+    private _movieConverter: MovieConverter;
+    private _movies: Array<Movie>;
+    private _movieNames: Array<String>;
 
     constructor() { }
 
     ngOnInit() {
         this._ticketOrder = new TicketOrder();
-        this._movies = new Array<String>();
-        this._movies.push("Zootopia");
-        this._movies.push("Captain America");
-        this._movies.push("The Jungle Book");
+
+        this._movies = new Array<Movie>();
+        this._movies.push(new Movie(123, "Zootopia"));
+        this._movies.push(new Movie(217, "Captain America"));
+        this._movies.push(new Movie(324, "The Jungle Book"));
+
+        this._movieConverter = new MovieConverter(this._movies);
     }
 
     get ticketOrder() {
         return this._ticketOrder;
+    }
+
+    get movieConverter() {
+        return this._movieConverter;
+    }
+
+    get movieNames() {
+        if (!this._movieNames) {
+            this._movieNames = this.movies.map((value: Movie) => value.name);
+        }
+        return this._movieNames;
     }
 
     get movies() {
