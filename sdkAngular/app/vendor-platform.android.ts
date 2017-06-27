179398c3d8afae31db808cbf069c06b4c44e9d45
vchimev <Vasil.Chimev@telerik.com>
2017-06-27 19:57:07 +0300

    Update to webpack@0.7.1

--- sdkAngular/app/vendor-platform.android.ts
+++ sdkAngular/app/vendor-platform.android.ts
@@ -9,8 +9,16 @@
 // placed here needs to be careful about its dependencies.
 
 require("application");
-require("ui/frame");
-require("ui/frame/activity");
+if (!global["__snapshot"]) {
+    // In case snapshot generation is enabled these modules will get into the bundle
+    // but will not be required/evaluated.
+    // The snapshot webpack plugin will add them to the tns-java-classes.js bundle file.
+    // This way, they will be evaluated on app start as early as possible.
+
+    require("ui/frame");
+    require("ui/frame/activity");
+    require("./main-activity.android.js");
+}
 
 if (global.TNS_WEBPACK) {
     global.__requireOverride = function (name, dir) {
@@ -24,4 +32,4 @@ if (global.TNS_WEBPACK) {
     };
 }
 
-require("./main-activity.android");
+
