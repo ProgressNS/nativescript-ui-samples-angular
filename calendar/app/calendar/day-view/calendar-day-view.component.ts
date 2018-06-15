b75df0aebbb36430ff5c79229c5adcc55032c0a4
Dimitar Tachev <Dimitar.Tachev@progress.com>
2018-06-15 17:38:10 +0300

    chore: fix calendar demo

--- calendar/app/calendar/day-view/calendar-day-view.component.ts
+++ calendar/app/calendar/day-view/calendar-day-view.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CalendarDayEventsService } from "../calendar-day-events.service";
-import { RadCalendar, CalendarEvent, CalendarDayViewEventSelectedData, CalendarDayViewStyle } from "nativescript-ui-calendar";
-import { RadCalendarComponent } from "nativescript-ui-calendar/angular";
+import { RadCalendar, CalendarEvent, CalendarDayViewEventSelectedData } from "nativescript-ui-calendar";
 import { alert } from "tns-core-modules/ui/dialogs";
 
 @Component({
@@ -13,10 +12,6 @@ import { alert } from "tns-core-modules/ui/dialogs";
 
 export class CalendarDayViewComponent implements OnInit {
     private _events: Array<CalendarEvent>;
-    private _style: CalendarDayViewStyle;
-    private _weekVisible: boolean;
-    private _titleVisible: boolean;
-    @ViewChild("myCalendar") _calendar: RadCalendarComponent;
 
     constructor(private _calendarService: CalendarDayEventsService) {
 
@@ -28,22 +23,6 @@ export class CalendarDayViewComponent implements OnInit {
 
     ngOnInit() {
         this._events = this._calendarService.getCalendarDayEvents();
-        this._style = new CalendarDayViewStyle();
-        this._calendar.nativeElement.dayViewStyle = this._style;
-        this._weekVisible = true;
-        this._titleVisible = true;
-    }
-
-    onWeekTap(args: any) {
-        this._weekVisible = !this._weekVisible;
-        this._style.showWeek = this._weekVisible;
-        args.object.text = this._weekVisible ? 'hide week' : 'show week';
-    }
-
-    onTitleTap(args: any) {
-        this._titleVisible = !this._titleVisible;
-        this._style.showTitle = this._titleVisible;
-        args.object.text = this._titleVisible ? 'hide title' : 'show title';
     }
 
     onDayViewEventSelected(args: CalendarDayViewEventSelectedData) {
